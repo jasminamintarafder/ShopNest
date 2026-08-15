9607db07cbfc3df7158ed8fdd1c94fef3e19ce04
Jasmin Amin Trafder <jasminamitarafder@gmail.com>
2026-08-15 22:45:10 +0600

    feat: enhance backend CORS configuration and add new frontend pages for About, Disclaimer, and Return Policy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ProductCard from "./components/ProductCard";
+import About from './pages/About';
+import Disclaimer from './pages/Disclaimer';
+import ReturnPolicy from './pages/ReturnPolicy';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="/return" element={<ReturnPolicy />} />
       </Routes>
       <Footer />
     </Router>
